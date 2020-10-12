6058fdf656fbad86dbfa788702eba22a36bcc4cd
ming <904933873@qq.com>
2020-10-12 22:14:10 +0800

    jjj

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}> Gina </h1>
+      <h1 className={styles.title}> 11Gina </h1>
     </div>
   );
 };
