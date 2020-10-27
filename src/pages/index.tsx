bb12f1e823d16901afcc2ab1a16ca39da355f08b
Chiming <chiming@netbank.cn>
2020-10-27 11:41:55 +0800

    tt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}> hi </h1>
+      <h1 className={styles.title}> hi again</h1>
     </div>
   );
 };
