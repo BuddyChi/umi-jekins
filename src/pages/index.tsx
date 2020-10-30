cab995f1baf381eb7374d19d593026c042685605
Chiming <chiming@netbank.cn>
2020-10-30 13:27:40 +0800

    youyo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}> hi again</h1>
+      <h1 className={styles.title}> youyou hi again</h1>
     </div>
   );
 };
